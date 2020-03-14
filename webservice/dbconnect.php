dfc355b916203853b05b3084177ffcc1fbab1fc9
future1507 <55175862+future1507@users.noreply.github.com>
2020-03-14 23:48:14 +0700

    webservice 1.0

---
+++ webservice/dbconnect.php
@@ -0,0 +1,10 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "rockneverdie1507";
+    $dbname = "demo";
+
+    $dbconn = new mysqli($servername,$username,$password,$dbname);
+
+
+?>
