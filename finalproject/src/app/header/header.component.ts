041681bd4679094aac70d10ff9891a57740e140a
future1507 <55175862+future1507@users.noreply.github.com>
2020-03-21 04:14:13 +0700

    new&show filecabinet

--- finalproject/src/app/header/header.component.ts
+++ finalproject/src/app/header/header.component.ts
@@ -1,28 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { HomeComponent } from '../home/home.component';
+
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor(private http: HttpClient,private router : Router) {}
+  Userid;
+  constructor(private http: HttpClient,private router : Router) {
+    this.Userid = HomeComponent.prototype.Userid;
+  }
   ngOnInit(): void {
   }
   Profile(){
-      this.router.navigateByUrl('/profile');
+      console.log(this.Userid);
+      this.router.navigateByUrl('/profile/'+this.Userid);
     }
   Home(){
-    this.router.navigateByUrl('/home');
+    this.router.navigateByUrl('/home/'+this.Userid);
   }
-  cabinet(){
-    this.router.navigateByUrl('/cabinet');
-  }
-  document(){
-    this.router.navigateByUrl('/document()');
-  }
+  // cabinet(){
+  //   this.router.navigateByUrl('/cabinet');
+  // }
+  // document(){
+  //   this.router.navigateByUrl('/document()');
+  // }
   
 
 }
