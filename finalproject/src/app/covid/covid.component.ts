77ab19601b1ac68993bcae0e3b0766bcd00f7716
Chollada <54612005+por031hub@users.noreply.github.com>
2020-04-04 03:20:21 +0700

    จะเช้าแล้วนะ

--- finalproject/src/app/covid/covid.component.ts
+++ finalproject/src/app/covid/covid.component.ts
@@ -3,6 +3,7 @@ import { SelectItem } from 'primeng/api/selectitem';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { DatapassService } from '../datapass.service';
+import { Time } from '@angular/common';
 
 
 
@@ -25,12 +26,24 @@ export class CovidComponent implements OnInit {
   genders: SelectItem[];
   date1: Date;
   displayBasic: boolean;
-  date=null;
-  time=null;
+  dates:SelectItem[];
+  times:SelectItem[];
+  date:Date;
+  time:Time;
   covids;
   date8: Date;
   Name=null;
   Career=null;
+  days:SelectItem[];
+  day:Date;
+  houres:SelectItem[];
+  houre:Time;
+  minutes:SelectItem[];
+  minute:Time;
+  monthes:SelectItem[];
+  month:Date;
+  years:SelectItem[];
+  year:Date;
   invalidDates: Array<Date>;
   constructor(private http: HttpClient,private router : Router
     ,private data : DatapassService) {
@@ -132,6 +145,152 @@ export class CovidComponent implements OnInit {
       {label: 'อุทัยธานี',value: 'อุทัยธานี'},
       {label: 'อุบลราชธานี',value: 'อุบลราชธานี'}];
 
+     this.houres = [
+      {label:'	0'	,value:'0'},
+      {label:'	1'	,value:'1'},
+      {label:'	2'	,value:'2'},
+      {label:'	3'	,value:'3'},
+      {label:'	4'	,value:'4'},
+      {label:'	5'	,value:'5'},
+      {label:'	6'	,value:'6'},
+      {label:'	7'	,value:'7'},
+      {label:'	8'	,value:'8'},
+      {label:'	9'	,value:'9'},
+      {label:'	10'	,value:'10'},
+      {label:'	11'	,value:'11'},
+      {label:'	12'	,value:'12'},
+      {label:'	13'	,value:'13'},
+      {label:'	14'	,value:'14'},
+      {label:'	15'	,value:'15'},
+      {label:'	16'	,value:'16'},
+      {label:'	17'	,value:'17'},
+      {label:'	18'	,value:'18'},
+      {label:'	19'	,value:'19'},
+      {label:'	20'	,value:'20'},
+      {label:'	21'	,value:'21'},
+      {label:'	22'	,value:'22'},
+      {label:'	23'	,value:'23'},
+      
+     ];
+     this.minutes = [
+      {label:'	0'	,value:'	0'	},
+      {label:'	1'	,value:'	1'  },
+      {label:'	2'	,value:'	2'  },
+      {label:'	3'	,value:'	3'	},
+      {label:'	4'	,value:'	4'	},
+      {label:'	5	' ,value:'	5'	},
+      {label:'	6'	,value:'	6'	},
+      {label:'	7'	,value:'	7'	},
+      {label:'	8'	,value:'	8'	},
+      {label:'	9'	,value:'	9'	},
+      {label:'	10'	,value:'	10'	},
+      {label:'	11'	,value:'	11'	},
+      {label:'	12'	,value:'	12'	},
+      {label:'	13'	,value:'	13'	},
+      {label:'	14'	,value:'	14'	},
+      {label:'	15'	,value:'	15'	},
+      {label:'	16'	,value:'	16'	},
+      {label:'	17'	,value:'	17' },
+      {label:'	18'	,value:'	18'	},
+      {label:'	19'	,value:'	19'	},
+      {label:'	20'	,value:'	20'	},
+      {label:'	21'	,value:'	21'	},
+      {label:'	22'	,value:'	22'	},
+      {label:'	23'	,value:'	23'	},
+      {label:'	24'	,value:'	24'	},
+      {label:'	25'	,value:'	25' },
+      {label:'	26'	,value:'	26'	},
+      {label:'	27'	,value:'	27'	},
+      {label:'	28'	,value:'	28'	},
+      {label:'	29'	,value:'	29'	},
+      {label:'	30'	,value:'	30'	},
+      {label:'	31'	,value:'	31'	},
+      {label:'	32'	,value:'	32'	},
+      {label:'	33'	,value:'	33'	},
+      {label:'	34'	,value:'	34'	},
+      {label:'	35'	,value:'	35'	},
+      {label:'	36'	,value:'	36'	},
+      {label:'	37'	,value:'	37'	},
+      {label:'	38'	,value:'	38'	},
+      {label:'	39'	,value:'	39'	},
+      {label:'	40'	,value:'	40'	},
+      {label:'	41'	,value:'	41'	},
+      {label:'	42'	,value:'	42'	},
+      {label:'	43'	,value:'	43'	},
+      {label:'	44'	,value:'	44'	},
+      {label:'	45'	,value:'	45'	},
+      {label:'	46'	,value:'	46'	},
+      {label:'	47'	,value:'	47'	},
+      {label:'	48'	,value:'	48'	},
+      {label:'	49'	,value:'	49'	},
+      {label:'	50'	,value:'	50'	},
+      {label:'	51'	,value:'	51'	},
+      {label:'	52'	,value:'	52'	},
+      {label:'	53'	,value:'	53'	},
+      {label:'	54'	,value:'	54'	},
+      {label:'	55'	,value:'	55'	},
+      {label:'	56'	,value:'	56'	},
+      {label:'	57'	,value:'	57'	},
+      {label:'	58'	,value:'	58'	},
+      {label:'	59'	,value:'	59'	},
+      
+     ];
+     this.days = [
+      {label:'	1'	,value:'	1'	},
+      {label:'	2'  ,value:'	2'	},
+      {label:'	3'	,value:'	3'	},
+      {label:'	4'	,value:'	4'	},
+      {label:'	5'	,value:'	5'	},
+      {label:'	6'	,value:'	6'	},
+      {label:'	7'	,value:'	7'	},
+      {label:'	8'	,value:'	8'	},
+      {label:'	9'	,value:'	9'	},
+      {label:'	10'	,value:'	10'	},
+      {label:'	11'	,value:'	11'	},
+      {label:'	12'	,value:'	12'	},
+      {label:'	13'	,value:'	13'	},
+      {label:'	14'	,value:'	14'	},
+      {label:'	15'	,value:'	15'	},
+      {label:'	16'	,value:'	16'	},
+      {label:'	17'	,value:'	17'	},
+      {label:'	18'	,value:'	18'	},
+      {label:'	19'	,value:'	19'	},
+      {label:'	20'	,value:'	20'	},
+      {label:'	21'	,value:'	21'	},
+      {label:'	22'	,value:'	22'	},
+      {label:'	23'	,value:'	23'	},
+      {label:'	24'	,value:'	24' },
+      {label:'	25'	,value:'	25'	},
+      {label:'	26'	,value:'	26'	},
+      {label:'	27'	,value:'	27'	},
+      {label:'	28'	,value:'	28'	},
+      {label:'	29'	,value:'	29'	},
+      {label:'	30'	,value:'	30'	},
+      {label:'	31'	,value:'	31'	},
+      
+     ];
+     this.monthes = [
+      {label:'	1'	,value:'	1'	},
+      {label:'	2'	,value:'	2'	},
+      {label:'	3'  ,value:'	3'	},
+      {label:'	4'	,value:'	4'	},
+      {label:'	5'	,value:'	5'	},
+      {label:'	6'	,value:'	6'	},
+      {label:'	7'	,value:'	7'	},
+      {label:'	8'	,value:'	8'	},
+      {label:'	9'	,value:'	9'	},
+      {label:'	10'	,value:'	10'	},
+      {label:'	11'	,value:'	11'	},
+      {label:'	12'	,value:'	12'	},
+      
+     ];
+     this.years = [
+      {label:'	2018'	,value:'	2018'	},
+      {label:'	2019'	,value:'	2019'	},
+      {label:'	2020'	,value:'	2020'	},
+      {label:'	2021'	,value:'	2021'	},
+      
+     ];
   }
   en: any;
    ngOnInit() {
@@ -144,7 +303,7 @@ export class CovidComponent implements OnInit {
       monthNamesShort: [ "Jan", "Feb", "Mar", "Apr", "May", "Jun","Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ],
       today: 'Today',
       clear: 'Clear',
-      dateFormat: 'mm/dd/yy',
+      dateFormat: 'dd/mm/yy',
       weekHeader: 'Wk'
   };
   let today = new Date();
@@ -165,7 +324,8 @@ export class CovidComponent implements OnInit {
                   Gender : this.gender,
                   Career : this.Career,
                   Province : this.province,
-                  Date:this.date.getDate()+"/"+this.date.getMonth()+"/"+this.date.getFullYear(),
+                  Date : this.date,
+                  // Date:this.date.getDate()+"/"+this.date.getMonth()+"/"+this.date.getFullYear(),
                   Time : this.time};
     console.log(json);
     let response = await this.http
